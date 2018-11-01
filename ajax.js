60971153a85cb8bf64050b76a2b05e8580f76b23
Silber <silber.ypm@gmail.com>
2018-11-01 06:38:07 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -18,34 +18,69 @@ window.onload = () => {
 
     // JSON   JavaScript Object Notetion
 
-    // https://github.com/smelukov/citiesTest/blob/master/cities.json
+    // https://github.com/smelukov/citiesTest/blob/master/cities.json    
 
-    const reqBtn = document.querySelector('#btn');
-    const resContent = document.querySelector('#response');
+    // const reqBtn = document.querySelector('#btn');
+    // const resContent = document.querySelector('#response');
    
-    reqBtn.addEventListener('click', function() {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');
-        xhr.send();
-        xhr.addEventListener('load', () => {
-            if (xhr.status == 200) {
-                console.log('Response received => ', xhr.response);
-                var resJ = JSON.parse(xhr.response);
-                resContent.textContent = xhr.response;
-                // for (var prop in resJ){
-                //     resContent.append(prop["userId"]);
-                // };
-                console.log('status: ' + xhr.status)
-            } else {
-                resContent.textContent = xhr.status;
-            }
+    // reqBtn.addEventListener('click', function() {
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');     
+    //     xhr.send();
+    //     xhr.addEventListener('load', () => {
+    //         if (xhr.status == 200) {
+    //             console.log('Response received => ', xhr.response);
+    //             var resJ = JSON.parse(xhr.response);
+    //             resContent.textContent = xhr.response;
+    //             // for (var prop in resJ){
+    //             //     resContent.append(prop["userId"]);
+    //             // };
+    //             console.log('status: ' + xhr.status)
+    //         } else {
+    //             resContent.textContent = xhr.status;
+    //         }
 
-        });
-    });
+    //     });
+    // });
 
 
     // To do same with Promise !!
 
+    const reqBtn = document.querySelector('#btn');
+    const resContent = document.querySelector('#response');
+
+    function loadFile(url) {                            //
+        return new Promise((resolve, reject) => {       // 
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url);                       // get with url
+            xhr.send();
+            xhr.addEventListener('load',() => { // on response load => promise resolve (response)
+                resolve(xhr.response);
+            });
+        });
+    }
+
+
+
+    reqBtn.addEventListener('click', function () {
+        loadFile('https://jsonplaceholder.typicode.com/todos/1')
+            .then((value) => {
+                resContent.textContent = value;
+                console.log(value);                     // value recieved from resolve param (Promise) 
+
+                return loadFile('https://jsonplaceholder.typicode.com/todos/2')
+            })   
+            .then((value) => {
+                resContent.textContent += value;
+                console.log(value);
+
+            return loadFile('https://jsonplaceholder.typicode.com/todos/3')
+            })
+            .then((value) => {
+                resContent.textContent += value;
+                console.log(value);
+            })
+    });
     
 
 
